c42aec14b1f1ba4b336fc9b11a15c473f1fc1a11
tad34s <tadeas.baca@email.cz>
2023-11-27 13:24:18 +0100

    More training examples

--- Python Trainer/variables.py
+++ Python Trainer/variables.py
@@ -3,8 +3,8 @@ max_trained_epochs = 500
 
 # Hyperparameters
 start_temperature = 20
-reduce_temperature = 1/10 # 0.75
-num_training_examples = 300 # 300
+reduce_temperature = start_temperature/400 # 400 = when we want the to start to be 0
+num_training_examples = 500 # 300
 discount = 0.95 # devalues future reward
 learning_rate = 0.0005
 
@@ -13,5 +13,5 @@ learning_rate = 0.0005
 reward_same_action = 2.0 # will be added to the reward for sticking with the same action
 
 # Discretization
-num_neurons = 10 # number of neurons in the output layer (per variable)
-disc_step_size = 1/num_neurons
+num_neurons = 2 # number of neurons in the output layer (per variable)
+disc_step_size = 1/(num_neurons-1)
