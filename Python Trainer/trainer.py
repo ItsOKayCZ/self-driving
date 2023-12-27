ce993bed6fc49b5e7d1c1d0ba0ba31ecfd4b20c2
tad34s <tadeas.baca@email.cz>
2023-12-27 02:19:56 +0100

    Speeding up evaluating

--- Python Trainer/trainer.py
+++ Python Trainer/trainer.py
@@ -84,19 +84,20 @@ class Trainer:
                 for agent_id, i in terminal_steps.agent_id_to_index.items():
                     exp = exps[agent_id]
                     exp.add_instance(terminal_steps[agent_id].obs,
-                                                None,
-                                                (np.zeros(self.model.output_shape_speed),
-                                                 np.zeros(self.model.output_shape_steer)),
-                                                terminal_steps[agent_id].reward)
+                                     None,
+                                     (np.zeros(self.model.output_shape_speed),
+                                      np.zeros(self.model.output_shape_steer)),
+                                     terminal_steps[agent_id].reward)
                     exp.rewards.pop(0)
                     all_rewards += sum(exp.rewards)
                     self.memory.add_exp(exp)
                     print(f"{len(self.memory) * 100 / self.memory.size}%")
-                    if len(self.memory) + n_active_agents <= self.memory.size:
-                        exps[agent_id] = Experience()
-                    else:
-                        n_active_agents -= 1
+                    if len(self.memory) + n_active_agents > self.memory.size:
+                        n_active_agents -= 0
                         terminated[agent_id] = True
+                    else:
+                        exps[agent_id] = Experience()
+
             else:
                 for agent_id, i in decision_steps.agent_id_to_index.items():
 
@@ -179,14 +180,16 @@ class Trainer:
 
     def evaluate(self, env, new_model: QNetwork, temperature: float) -> bool:
         print("------ Evaluating ------")
-        old_model_scores = [0] * self.num_evaluations
-        new_model_scores = [0] * self.num_evaluations
+
         print("Testing new model...")
-        self.fill_scores(env, new_model, new_model_scores)
+        new_model_scores = self.fill_scores(env, new_model)
+
         print("Testing old model...")
-        self.fill_scores(env, self.model, old_model_scores)
+        old_model_scores = self.fill_scores(env, self.model)
+
         new_model_fitness_score = sum(new_model_scores) / self.num_evaluations
         old_model_fitness_score = sum(old_model_scores) / self.num_evaluations
+
         print(f"New model score: {new_model_fitness_score}, Old model score: {old_model_fitness_score}")
         if new_model_fitness_score >= old_model_fitness_score:
             print("Updating model...")
@@ -198,53 +201,51 @@ class Trainer:
             print("Not updating model...")
             return False
 
-    def fill_scores(self, env, model: QNetwork, scores_list: list):
-
+    def fill_scores(self, env, model: QNetwork) -> list:
+        score_list = []
+        scores = [0 for _ in range(self.num_agents)]
+        n_active_agents = self.num_agents
+        terminated = [False for _ in range(self.num_agents)]
         behavior_name = list(env.behavior_specs)[0]
-        next_index_to_fill = 0
-        terminated = [False] * self.num_evaluations
-        while not all(terminated):
 
-            env.reset()  # for new tracks
+        while len(score_list) != self.num_evaluations:
 
-            print(f"{next_index_to_fill * 100 / self.num_evaluations}%")
-            while True:
-                decision_steps, terminal_steps = env.get_steps(behavior_name)
+            decision_steps, terminal_steps = env.get_steps(behavior_name)
 
-                dis_action_values = []
-                cont_action_values = []
+            dis_action_values = []
+            cont_action_values = []
 
-                if len(decision_steps) == 0:
-                    for agent_id, i in terminal_steps.agent_id_to_index.items():
-                        if agent_id + next_index_to_fill < len(scores_list):
-                            scores_list[agent_id + next_index_to_fill] += terminal_steps[agent_id].reward
-                            terminated[agent_id + next_index_to_fill] = True
+            if len(decision_steps) == 0:
+                for agent_id, i in terminal_steps.agent_id_to_index.items():
+                    score_list.append(scores[agent_id])
+                    if n_active_agents + len(score_list) > self.num_evaluations:
+                        n_active_agents -= 1
+                        terminated[agent_id] = True
+                    else:
+                        scores[agent_id] = 0
+                    print(f"{len(score_list) * 100 / self.num_evaluations}%")
+            else:
 
-                else:
+                for agent_id, i in decision_steps.agent_id_to_index.items():
+                    if terminated[agent_id]:
+                        dis_action_values.append(np.array([]))
+                        cont_action_values.append([0, 0])
+                        continue
+                    # Get the action
 
-                    for agent_id, i in decision_steps.agent_id_to_index.items():
-                        if agent_id + next_index_to_fill >= len(scores_list) or terminated[agent_id + next_index_to_fill]:
-                            dis_action_values.append(np.array([]))
-                            cont_action_values.append([0, 0])
-                            continue
-                        # Get the action
-                        q_values, actions, indices = model.get_actions(decision_steps[i].obs, 0.5)
-                        scores_list[agent_id + next_index_to_fill] += decision_steps[i].reward
-                        dis_action_values.append([])
-                        cont_action_values.append(actions)
+                    q_values, actions, indices = model.get_actions(decision_steps[i].obs, 0.5)
+                    scores[agent_id] += decision_steps[i].reward
+                    dis_action_values.append([])
+                    cont_action_values.append(actions)
 
-                    action_tuple = ActionTuple()
-                    final_dis_action_values = np.array(dis_action_values)
-                    final_cont_action_values = np.array(cont_action_values)
-                    action_tuple.add_discrete(final_dis_action_values)
-                    action_tuple.add_continuous(final_cont_action_values)
-                    env.set_actions(behavior_name, action_tuple)
-                env.step()
-                if all(terminated) or all(
-                        terminated[i] for i in range(next_index_to_fill, next_index_to_fill + self.num_agents)):
-                    break
+                action_tuple = ActionTuple()
+                final_dis_action_values = np.array(dis_action_values)
+                final_cont_action_values = np.array(cont_action_values)
+                action_tuple.add_discrete(final_dis_action_values)
+                action_tuple.add_continuous(final_cont_action_values)
+                env.set_actions(behavior_name, action_tuple)
 
-            next_index_to_fill += self.num_agents
+            env.step()
 
     def save_model(self, path):
         torch.onnx.export(
