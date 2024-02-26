6a82ffaa71ad7023f981d41641101f9019270a62
Oldřich Krupauer <oldrich.krupauer@pm.me>
2024-02-26 12:07:41 +0100

    Added speed to observations | python not working yet

--- Assets/Scripts/AgentCar.cs
+++ Assets/Scripts/AgentCar.cs
@@ -83,6 +83,7 @@ public class AgentCar : Agent
 	public override void CollectObservations(VectorSensor sensor)
 	{
 		sensor.AddObservation(carController.steeringAxis);
+		sensor.AddObservation(Mathf.Clamp(carController.carSpeed / carController.maxSpeed, 0f, 1f));
 	}
 
 	private GameObject getNextCheckpoint()
