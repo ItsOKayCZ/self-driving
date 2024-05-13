e0f4b87b094d90561e629a81c6971ab3c8cde0da
Tadeáš Báča <bacatade@fit.cvut.cz>
2024-05-13 13:40:24 +0200

    Adding maxpooling

--- Python Trainer/Network.py
+++ Python Trainer/Network.py
@@ -26,12 +26,18 @@ class QNetwork(torch.nn.Module):
             self.visual_input_shape = visual_input_shape
             self.nonvis_input_shape = nonvis_input_shape
             # calculating required size of the dense layer based on the conv layers
-            conv_1_hw = self.conv_output_shape((height, width), 5, 1)
-            conv_2_hw = self.conv_output_shape(conv_1_hw, 3, 1)
-            self.final_flat = conv_2_hw[0] * conv_2_hw[1] * 32
+            conv1_hw = self.conv_output_shape((height, width), 5, 1)
+            maxpool1_hw = self.conv_output_shape(conv1_hw, 2, 2)
+            conv2_hw = self.conv_output_shape(maxpool1_hw, 3, 1)
+            maxpool2_hw = self.conv_output_shape(conv2_hw, 2, 2)
+            self.final_flat = maxpool2_hw[0] * maxpool2_hw[1] * 32
+            print(self.final_flat)
+
             # layers
             self.conv1 = torch.nn.Conv2d(initial_channels, 16, 5)
+            self.maxpool1 = torch.nn.MaxPool2d(2, 2)
             self.conv2 = torch.nn.Conv2d(16, 32, 3)
+            self.maxpool2 = torch.nn.MaxPool2d(2, 2)
             self.nonvis_dense = torch.nn.Linear(nonvis_input_shape[0], 8)
             self.dense1 = torch.nn.Linear(self.final_flat + 8, encoding_size)
             self.dense2 = torch.nn.Linear(encoding_size, self.output_shape[1])
@@ -41,9 +47,13 @@ class QNetwork(torch.nn.Module):
         nonvis_obs = nonvis_obs.view((-1, self.nonvis_input_shape[0]))
 
         conv_1 = torch.relu(self.conv1(visual_obs))
-        conv_2 = torch.relu(self.conv2(conv_1))
+        maxpool1 = self.maxpool1(conv_1)
+        conv_2 = torch.relu(self.conv2(maxpool1))
+        maxpool2 = self.maxpool2(conv_2)
+
         nonvis_dense = torch.relu(self.nonvis_dense(nonvis_obs))
-        hidden = conv_2.reshape([-1, self.final_flat])
+
+        hidden = maxpool2.reshape([-1, self.final_flat])
         hidden = torch.concat([hidden, nonvis_dense], dim=1)
         hidden = self.dense1(hidden)
         hidden = torch.relu(hidden)
