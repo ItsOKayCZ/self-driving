b1d3c6f1d2109ad6a73f97bc0fe78daad7a760d9
tad34s <119794635+tad34s@users.noreply.github.com>
2024-10-25 11:22:49 +0200

    Unity reflections fix (#54)
    * Added shader for reflections from newest commit to fix problem with freeze
    
    * CI: Add build
    
    ---------
    
    Co-authored-by: Oldrich Krupauer <oldrich.krupauer@pm.me>
    Co-authored-by: ItsOKayCZ <ItsOKayCZ@users.noreply.github.com>

--- unity_env/Assets/Scripts/TrainingReplicator.cs
+++ unity_env/Assets/Scripts/TrainingReplicator.cs
@@ -82,6 +82,20 @@ namespace Unity.MLAgents.Areas
             ChangeCameraBackgroundColor();
         }
 
+        void SetMaterial()
+        {
+            foreach (var mat in roadMaterials)
+            {
+                mat.SetFloat(
+                    "_ReflectionStrength",
+                    DataChannel.getParameter("reflectionStrength", 1f)
+                );
+                mat.SetFloat("_NoiseScaleX", DataChannel.getParameter("noiseScaleX", 0.4f));
+                mat.SetFloat("_NoiseScaleY", DataChannel.getParameter("noiseScaleY", 0.05f));
+                mat.SetFloat("_Speed", DataChannel.getParameter("noiseSpeed", 0.3f));
+            }
+        }
+
         void Update()
         {
             if (randomBackgroundColor)
